6511a4c819128da3e3934b0098e37b4d7d43be60
hengchu <hengchu.zhang@gmail.com>
2018-11-27 16:13:54 -0500

    better and simplier encoding with syntax sort checking

--- src/SyntaxExt.hs
+++ src/SyntaxExt.hs
@@ -2,12 +2,61 @@ module SyntaxExt where
 
 import Syntax (Var, Binop(..))
 
+import Type.Reflection
 import Data.List (intercalate)
 import Data.Either (either)
+import qualified Data.Set as S
 import Data.Comp
 import Data.Comp.Show ()
 import Data.Comp.Equality ()
 import Data.Comp.Derive
+import Data.Comp.Variables
+import Data.Comp.Mapping
+
+data C = C deriving (Typeable, Show, Eq)
+data E = E deriving (Typeable, Show, Eq)
+
+data ExtVar :: * -> * -> * where
+  CExtVar :: Var -> ExtVar C a
+  EExtVar :: Var -> ExtVar E a
+
+data AnyExtVar :: * where
+  AnyExtVar :: (Typeable s) => ExtVar s a -> AnyExtVar
+
+varSort :: forall s a. (Typeable s) => ExtVar s a -> TypeRep s
+varSort _ = typeRep @s
+
+instance Eq AnyExtVar where
+  a == b =
+    case (a, b) of
+      (AnyExtVar a', AnyExtVar b') ->
+        case eqTypeRep (varSort a') (varSort b') of
+          Just HRefl ->
+            case (a', b') of
+              (CExtVar a'', CExtVar b'') -> a'' == b''
+              (EExtVar a'', EExtVar b'') -> a'' == b''
+          _ -> False
+
+instance Ord AnyExtVar where
+  compare a b =
+    case (a, b) of
+      (AnyExtVar a', AnyExtVar b') ->
+        case eqTypeRep (varSort a') (varSort b') of
+          Just HRefl ->
+            case (a', b') of
+              (CExtVar a'', CExtVar b'') -> compare a'' b''
+              (EExtVar a'', EExtVar b'') -> compare a'' b''
+          _ ->
+            case (a', b') of
+              (EExtVar _,   CExtVar _) -> Prelude.LT
+              (CExtVar _,   EExtVar _) -> Prelude.GT
+              _ -> error "impossible case"
+
+deriving instance Show    (ExtVar s a)
+deriving instance Functor (ExtVar s)
+deriving instance Eq      (ExtVar s a)
+deriving instance Ord     (ExtVar s a)
+deriving instance Show    AnyExtVar
 
 data Lit e = LInt Int
            | LFloat Float
@@ -16,318 +65,284 @@ data Lit e = LInt Int
            | LBag [e]
   deriving (Functor, Show, Eq, Foldable, Traversable)
 
-data ExtVar a = ExtVar Var
-  deriving Functor
-
 data EVar e = EVar Var
-  deriving Functor
+  deriving (Functor)
 data ELength e = ELength e
-  deriving Functor
+  deriving (Functor)
 data ELit e = ELit (Lit e)
-  deriving Functor
+  deriving (Functor)
 data EBinop e = EBinop Binop e e
-  deriving Functor
+  deriving (Functor)
 data EIndex e = EIndex e e
-  deriving Functor
+  deriving (Functor)
 data ERAccess e = ERAccess e String
-  deriving Functor
+  deriving (Functor)
 data EFloat e = EFloat e
-  deriving Functor
+  deriving (Functor)
 data EExp e = EExp e
-  deriving Functor
+  deriving (Functor)
 data ELog e = ELog e
-  deriving Functor
+  deriving (Functor)
 data EClip e = EClip e (Lit e)
-  deriving Functor
+  deriving (Functor)
 data EScale e = EScale e e
-  deriving Functor
+  deriving (Functor)
 data EDot e = EDot e e
-  deriving Functor
+  deriving (Functor)
 
 type Expr = EVar :+: ELength :+: ELit
             :+: EBinop :+: EIndex :+: ERAccess
             :+: EFloat :+: EExp :+: ELog
             :+: EClip :+: EScale :+: EDot
-type Expr' = ExtVar :+: Expr
+type Expr' = ExtVar E :+: Expr
 
-$(derive [makeTraversable, makeFoldable, makeEqF, makeShowF, smartConstructors, smartAConstructors]
+$(derive [makeTraversable, makeFoldable, makeEqF,
+          makeShowF, smartConstructors, smartAConstructors]
          [''EVar, ''ELength, ''ELit, ''EBinop,
           ''EIndex, ''ERAccess, ''EFloat, ''EExp,
           ''ELog, ''EClip, ''EScale, ''EDot,
           ''ExtVar])
 
-data ParamE e = ParamE e
-  deriving Functor
-data ParamC c = ParamC c
-  deriving Functor
-
-$(derive [makeTraversable, makeFoldable, makeEqF, makeShowF, smartConstructors, smartAConstructors]
-         [''ParamE, ''ParamC])
-
-data CAssign e c = CAssign e e
+data CAssign e = CAssign e e
   deriving (Functor)
-data CLaplace e c = CLaplace Var Float e
+data CLaplace e = CLaplace Var Float e
   deriving (Functor)
-data CIf e c = CIf e c c
+data CIf c = CIf c c c
   deriving (Functor)
-data CWhile e c = CWhile e c
+data CWhile c = CWhile c c
   deriving (Functor)
 data CSeq c = CSeq c c
   deriving (Functor)
 data CSkip c = CSkip
   deriving (Functor)
-data CExt e c = CExt String [Either (ParamE e) (ParamC c)]
-  deriving (Functor, Foldable, Traversable)
+data CExt c = CExt String [c]
+  deriving (Functor)
+data CExtDecl c = CExtDecl String [AnyExtVar] c
+  deriving (Functor)
 
-type GImp e = CAssign (Term e) :+: CLaplace (Term e) :+: CIf (Term e)
-              :+: CWhile (Term e) :+: CSeq :+: CSkip
-
--- |The vanilla Imp language
-type Imp  = GImp Expr
+-- The vanilla IMP language
+type Imp = CAssign :+: CLaplace :+: CIf
+           :+: CWhile :+: CSeq :+: CSkip :+: Expr
 
 -- |The extended Imp language with extensions
-type Imp' = ExtVar :+: (CExt (Term Expr')) :+: (GImp Expr')
+type Imp' = ExtVar C :+: CExtDecl :+: CExt :+: Expr'
 
-$(derive [makeTraversable, makeFoldable, makeEqF, makeShowF, smartConstructors, smartAConstructors]
-         [''CAssign, ''CLaplace, ''CIf, ''CWhile, ''CSeq, ''CSkip])
+$(derive [makeTraversable, makeFoldable, makeEqF, makeShowF,
+          smartConstructors, smartAConstructors]
+         [''CAssign, ''CLaplace, ''CIf, ''CWhile,
+          ''CSeq, ''CSkip, ''CExt, ''CExtDecl])
 
-iCExt :: (CExt e :<: f) => String -> [Either (ParamE e) (ParamC (Cxt h f a))] -> Cxt h f a
-iCExt name params = inject $ CExt name params
+type Sort = Either C E
 
-instance (Functor e, ShowF e) => ShowF (CExt (Term e)) where
-  showF (CExt name params) =
-    "CExt " ++ name ++ " " ++ (intercalate " " . map (either paramE paramC) $ params)
-    where paramE :: ParamE (Term e) -> String
-          paramE = showF . (fmap show)
+class Wellformed f where
+  syntaxSort :: AlgM Maybe f Sort
 
-          paramC :: ParamC String -> String
-          paramC = showF
+$(derive [liftSum] [''Wellformed])
 
--- |An extension declaration, with its name, bound variables, and the expansion
--- definition.
-data CExtDecl = CExtDecl String [Var] (Term Imp')
+instance Wellformed EVar where
+  syntaxSort _ = return . Right $ E
 
-class SubstE f t g where
-  substE :: Var -> ParamE (Term t) -> Alg f (Term g)
+instance Wellformed ELength where
+  syntaxSort (ELength s) = do
+    case s of
+      Left _ -> Nothing
+      Right E -> return . Right $ E
 
-class Subst f p t g where
-  subst :: Var -> p (Term t) -> Alg f (Term g)
+instance Wellformed ELit where
+  syntaxSort (ELit (LArr sorts)) = do
+    if all (== (Right E)) sorts
+      then return . Right $ E
+      else Nothing
+  syntaxSort (ELit (LBag sorts)) = do
+    if all (== (Right E)) sorts
+      then return . Right $ E
+      else Nothing
+  syntaxSort _ = return . Right $ E
 
-instance {-# OVERLAPPING #-}
-         ( Functor t
-         , SubstE f1 t g
-         , SubstE f2 t g
-         ) => SubstE (f1 :+: f2) t g where
-  substE x p =
-    caseF
-      (substE @f1 @t @g x p)
-      (substE @f2 @t @g x p)
+instance Wellformed EBinop where
+  syntaxSort (EBinop _ s1 s2) = do
+    case (s1, s2) of
+      (Right E, Right E) -> return $ Right E
+      _ -> Nothing
 
-instance {-# OVERLAPPING #-}
-         ( Functor t
-         , Subst f1 ParamE t g
-         , Subst f2 ParamE t g
-         ) => Subst (f1 :+: f2) ParamE t g where
-  subst x p =
-    caseF
-      (subst @f1 @ParamE @t @g x p)
-      (subst @f2 @ParamE @t @g x p)
+instance Wellformed EIndex where
+  syntaxSort (EIndex s1 s2) = do
+    case (s1, s2) of
+      (Right E, Right E) -> return $ Right E
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-}
-         ( ExtVar :<: expr
-         , Functor t
-         , t      :<: expr
-         ) => SubstE ExtVar t expr where
-  substE x (ParamE t) = \e ->
-    case e of
-      ExtVar y | x == y -> deepInject t
-      _ -> inject e
+instance Wellformed ERAccess where
+  syntaxSort (ERAccess s _) = do
+    if s == Right E
+      then return $ Right E
+      else Nothing
 
-instance {-# OVERLAPPABLE #-} EVar :<: e => SubstE EVar t e where
-  substE _ _ = inject
+instance Wellformed EFloat where
+  syntaxSort (EFloat s) = do
+    if s == Right E
+      then return $ Right E
+      else Nothing
 
-instance {-# OVERLAPPABLE #-} ( ELength :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE ELength t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed EExp where
+  syntaxSort (EExp s) = do
+    if s == Right E
+      then return $ Right E
+      else Nothing
 
-instance {-# OVERLAPPABLE #-} ( ELit :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE ELit t e where
-  substE x p = \e ->
-    case e of
-      ELit le -> iELit $ fmap (substE x p . unTerm) le
+instance Wellformed ELog where
+  syntaxSort (ELog s) = do
+    if s == Right E
+      then return $ Right E
+      else Nothing
 
-instance {-# OVERLAPPABLE #-} ( EBinop :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EBinop t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed EClip where
+  syntaxSort (EClip s1 (LArr sorts)) = do
+    case (s1, all (== Right E) sorts) of
+      (Right E, True) -> return $ Right E
+      _ -> Nothing
+  syntaxSort (EClip s1 (LBag sorts)) = do
+    case (s1, all (== Right E) sorts) of
+      (Right E, True) -> return $ Right E
+      _ -> Nothing
+  syntaxSort (EClip s _) = do
+    if s == Right E then return $ Right E else Nothing
 
-instance {-# OVERLAPPABLE #-} ( EIndex :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EIndex t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed EScale where
+  syntaxSort (EScale s1 s2) = do
+    case (s1, s2) of
+      (Right E, Right E) -> return $ Right E
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-} ( ERAccess :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE ERAccess t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed EDot where
+  syntaxSort (EDot s1 s2) = do
+    case (s1, s2) of
+      (Right E, Right E) -> return $ Right E
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-} ( EFloat :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EFloat t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed (ExtVar E) where
+  syntaxSort _ = return $ Right E
 
-instance {-# OVERLAPPABLE #-} ( EExp :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EExp t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed (ExtVar C) where
+  syntaxSort _ = return $ Left C
 
-instance {-# OVERLAPPABLE #-} ( ELog :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE ELog t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed CAssign where
+  syntaxSort (CAssign s1 s2) =
+    case (s1, s2) of
+      (Right E, Right E) -> return $ Left C
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-} ( EScale :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EScale t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed CLaplace where
+  syntaxSort (CLaplace _ _ s) =
+    case s of
+      Right E -> return $ Left C
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-} ( EDot :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EDot t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed CIf where
+  syntaxSort (CIf s1 s2 s3) =
+    case (s1, s2, s3) of
+      (Right E, Left C, Left C) -> return $ Left C
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-} ( EClip :<: e
-         , Functor t
-         , t :<: e
-         , SubstE e t e
-         ) => SubstE EClip t e where
-  substE x p = inject . (fmap (substE x p . unTerm))
+instance Wellformed CWhile where
+  syntaxSort (CWhile s1 s2) =
+    case (s1, s2) of
+      (Right E, Left C) -> return $ Left C
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-}
-         ( ExtVar :<: cmd
-         , Functor t
-         , t      :<: cmd
-         ) => Subst ExtVar ParamC t cmd where
-  subst x (ParamC t) = \c ->
-    case c of
-      ExtVar y | x == y -> deepInject t
-      _ -> inject c
+instance Wellformed CSeq where
+  syntaxSort (CSeq s1 s2) =
+    case (s1, s2) of
+      (Left C, Left C) -> return $ Left C
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-}
-         ( ExtVar :<: cmd
-         ) => Subst ExtVar ParamE t cmd where
-  subst _ _ = inject
+instance Wellformed CSkip where
+  syntaxSort _ = return $ Left C
 
-instance {-# OVERLAPPABLE #-} ( Functor e
-         , SubstE e t e
-         , t :<: e
-         , CAssign (Term e)  :<: cmd
-         ) => Subst (CAssign (Term e)) ParamE t cmd where
-  subst x p = \c ->
-    case c of
-      CAssign lhs rhs ->
-        iCAssign (cata (substE @e @t @e x p) lhs) (cata (substE @e @t @e x p) rhs)
+instance Wellformed CExt where
+  syntaxSort (CExt _ _) = return $ Left C
 
-instance {-# OVERLAPPABLE #-} ( Functor e
-         , SubstE e t e
-         , t :<: e
-         , CLaplace (Term e) :<: cmd
-         ) => Subst (CLaplace (Term e)) ParamE t cmd where
-  subst x p = \c ->
-    case c of
-      CLaplace y w e ->
-        iCLaplace y w (cata (substE @e @t @e x p) e)
+instance Wellformed CExtDecl where
+  syntaxSort (CExtDecl _ _ s) =
+    case s of
+      Left C -> return $ Left C
+      _ -> Nothing
 
-instance {-# OVERLAPPABLE #-} ( Functor e
-         , Functor cmd
-         , SubstE e t e
-         , Subst cmd ParamE t cmd
-         , t :<: e
-         , CIf (Term e)  :<: cmd
-         ) => Subst (CIf (Term e)) ParamE t cmd where
-  subst x p = \c ->
-    case c of
-      CIf e c1 c2 ->
-        iCIf (cata (substE @e @t @e x p) e)
-             ((subst @cmd @_ @t @cmd x p) . unTerm $ c1)
-             ((subst @cmd @_ @t @cmd x p) . unTerm $ c2)
+{-
 
-instance {-# OVERLAPPABLE #-} ( Functor e
-         , Functor cmd
-         , SubstE e t e
-         , Subst cmd ParamE t cmd
-         , t :<: e
-         , CWhile (Term e) :<: cmd
-         ) => Subst (CWhile (Term e)) ParamE t cmd where
-  subst x p = \c ->
-    case c of
-      CWhile e body ->
-        iCWhile (cata (substE @e @t @e x p) e)
-                ((subst @cmd @_ @t @cmd x p) . unTerm $ body)
+instance HasVars (ExtVar E) (ExtVar E e) where
+  isVar a =
+    case a of
+      EExtVar v -> Just (EExtVar v)
 
-instance {-# OVERLAPPABLE #-} ( Functor cmd
-         , Subst cmd ParamE t cmd
-         , CSeq :<: cmd
-         ) => Subst CSeq ParamE t cmd where
-  subst x p = \c ->
-    case c of
-      CSeq c1 c2 ->
-        iCSeq ((subst @cmd @_ @t @cmd x p) . unTerm $ c1)
-              ((subst @cmd @_ @t @cmd x p) . unTerm $ c2)
+instance HasVars EVar     (ExtVar E e)
+instance HasVars ELength  (ExtVar E e)
+instance HasVars ELit     (ExtVar E e)
+instance HasVars EBinop   (ExtVar E e)
+instance HasVars EIndex   (ExtVar E e)
+instance HasVars ERAccess (ExtVar E e)
+instance HasVars EFloat   (ExtVar E e)
+instance HasVars EExp     (ExtVar E e)
+instance HasVars ELog     (ExtVar E e)
+instance HasVars EClip    (ExtVar E e)
+instance HasVars EScale   (ExtVar E e)
+instance HasVars EDot     (ExtVar E e)
 
-instance {-# OVERLAPPABLE #-} (CSkip :<: cmd) => Subst CSkip ParamE t cmd where
-  subst _ _ = inject
+instance HasVars (ExtVar C) (ExtVar C c) where
+  isVar a =
+    case a of
+      CExtVar v -> Just (CExtVar v)
 
-instance {-# OVERLAPPABLE #-} ( Functor e
-         , Functor cmd
-         , SubstE e t e
-         , Subst cmd ParamE t cmd
-         , t :<: e
-         , CExt (Term e)  :<: cmd
-         ) => Subst (CExt (Term e)) ParamE t cmd where
-  subst x p = \c ->
-    case c of
-      CExt nm params ->
-        iCExt nm (map (either paramE paramC) params)
-    where paramE :: ParamE (Term e) -> Either (ParamE (Term e)) any
-          paramE (ParamE term) = Left . ParamE $ cata (substE @e @t @e x p) term
+instance HasVars (ExtVar C)   (ExtVar E e)
+instance HasVars (CAssign e)  (ExtVar E e)
+instance HasVars (CLaplace e) (ExtVar E e)
+instance HasVars (CIf e)      (ExtVar E e)
+instance HasVars (CWhile e)   (ExtVar E e)
+instance HasVars CSeq         (ExtVar E e)
+instance HasVars CSkip        (ExtVar E e)
+instance HasVars (CExt e)     (ExtVar E e)
+instance HasVars CExtDecl     (ExtVar E e) where
+  bindsVars (CExtDecl _ vars c) = c |-> foldr iter S.empty vars
+    where iter :: AnyExtVar -> S.Set (ExtVar E c) -> S.Set (ExtVar E c)
+          iter v bvs =
+            case v of
+              AnyExtVar v' ->
+                case eqTypeRep (varSort v') (typeRep @E) of
+                  Just HRefl ->
+                    case v' of
+                      EExtVar v'' -> S.insert (EExtVar v'') bvs
+                  _ -> bvs
 
-          paramC :: ParamC (Term cmd) -> Either any (ParamC (Term cmd))
-          paramC (ParamC term) = Right . ParamC . (subst @cmd @_ @t @cmd x p) . unTerm $ term
-
-substEC :: Var -> ParamE (Term Expr') -> Term Imp' -> Term Imp'
-substEC x p c = cata (subst @Imp' @ParamE @Expr' @Imp' x p) c
+instance HasVars (CAssign e)  (ExtVar C c)
+instance HasVars (CLaplace e) (ExtVar C c)
+instance HasVars (CIf e)      (ExtVar C c)
+instance HasVars (CWhile e)   (ExtVar C c)
+instance HasVars CSeq         (ExtVar C c)
+instance HasVars CSkip        (ExtVar C c)
+instance HasVars (CExt e)     (ExtVar C c)
+instance HasVars CExtDecl     (ExtVar C c) where
+  bindsVars (CExtDecl _ vars c) = c |-> foldr iter S.empty vars
+    where  iter :: AnyExtVar -> S.Set (ExtVar C c) -> S.Set (ExtVar C c)
+           iter v bvs =
+            case v of
+              AnyExtVar v' ->
+                case eqTypeRep (varSort v') (typeRep @C) of
+                  Just HRefl ->
+                    case v' of
+                      CExtVar v'' -> S.insert (CExtVar v'') bvs
+                  _ -> bvs
+-}
 
 e1 :: Term Expr'
-e1 = iEBinop PLUS (iELit . LInt $ 1) (iExtVar "x")
+e1 = iEBinop PLUS (iELit (LInt 1)) (iEExtVar "x")
 
 e2 :: Term Expr'
-e2 = iEVar "y"
+e2 = iEBinop MINUS (iEVar "y") (iEVar "z")
 
-c1 :: Term Imp'
-c1 = iCAssign e2 e1
+e3 :: Term Imp'
+e3 = iEBinop PLUS (iELit (LInt 1)) (iCExtVar "x")
 
-c2 :: Term Imp'
-c2 = iCExt "bmap" [Left . ParamE $ e1, Right . ParamC $ c1]
+--e3 :: Term Expr'
+--e3 = substVars (\v -> if v == (EExtVar "x") then Just e2 else Nothing) e1
+
+--c1 :: Term Imp'
+--c1 = iCAssign e1 e2
