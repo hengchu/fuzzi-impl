e60112c39e6c40babdbbf76137e14e6dba7a9060
hengchu <hengchu.zhang@gmail.com>
2019-01-21 12:57:16 -0500

    some assertions to make sure we are computing things correctly

--- irisAccuracy.py
+++ irisAccuracy.py
@@ -18,12 +18,13 @@ total_examples = len(part[0])  + len(part[1]) + len(part[2])
 #Calculating accuracy
 err = 0
 for j in range(0,3):
-	labs = [part[j][i][4] for i in range(len(part[j]))]
-	lab0 = labs.count(0)
-	lab1 = labs.count(1)
-	lab2 = labs.count(2)
-	m = max(lab0, lab1, lab2)
-	err = err + len(part[j]) - m
+        labs = [part[j][i][4] for i in range(len(part[j]))]
+        lab0 = labs.count(0)
+        lab1 = labs.count(1)
+        lab2 = labs.count(2)
+        m = max(lab0, lab1, lab2)
+        assert(len(part[j]) - lab0 - lab1 - lab2 == 0)
+        err = err + len(part[j]) - m
 
 print ("errors: " + str(err))
 
